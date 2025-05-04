5641c9a314b84e5cee882d8ef824a997aa0df7ed
GiHoon Jang <stb.jang@gmail.com>
2025-05-04 09:20:39 +0900

    Create babel.config.js

---
+++ frontend/tests/babel.config.js
@@ -0,0 +1,9 @@
+// frontend/tests/babel.config.js
+// Babel configuration file
+
+module.exports = {
+    presets: [
+        '@babel/preset-env', // Transpile modern JavaScript
+        '@babel/preset-react', // Transpile JSX and React syntax
+    ],
+};
