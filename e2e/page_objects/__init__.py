c1215d62943900da25435fada78a780e5ed4e610
GiHoon Jang <stb.jang@gmail.com>
2025-05-04 10:51:17 +0900

    Update __init__.py

--- e2e/page_objects/__init__.py
+++ e2e/page_objects/__init__.py
@@ -1,1 +1,6 @@
+# e2e/page_objects/__init__.py
+# This file makes page_objects a Python package.
 
+from .base_page import BasePage
+from .main_page import MainPage
+# Import other page objects here
